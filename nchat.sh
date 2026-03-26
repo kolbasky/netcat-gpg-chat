ccc8ca9d938688aaa77232fd67d3f9f7c18a55be
kolbasky <42576088+kolbasky@users.noreply.github.com>
2026-03-26 03:15:33 +0300

    Update nchat.sh

--- nchat.sh
+++ nchat.sh
@@ -56,16 +56,18 @@ trap "tput rmcup; tput sgr0; rm -f \"$publickeyfile\"; exit 0;" EXIT
 # we listen for partner's key, write it to tempfile and import
 handshake() {
     nc -l $port | base64 -d > "${publickeyfile}"
-    partner_key=$(gpg --import "${publickeyfile}" 2>&1 | grep 'gpg: key' | grep -Eo '".*"' | tr -d '"')
+    partner_key_name=$(gpg --import "${publickeyfile}" 2>&1 | grep 'gpg: key' | grep -Eo '".*"' | tr -d '"')
 }
 # do it in background
 handshake &
 handshake_pid=$!
 
+# generate keypair for current user if not exists
 if [[ $( gpg --list-secret-keys $username 2> /dev/null | grep -c "^sec" ) -lt 1 ]]; then
     echo "⚠️  No GPG key found. Creating one..."
     gpg --quick-generate-key --pinentry-mode=loopback $username
 fi
+clear
 
 # try to send our public key to client
 echo -n "Waiting for partner $host:$port to become online"
@@ -73,28 +75,31 @@ until (timeout 15 gpg --armor --export $(gpg --list-secret-keys --with-colons $u
   echo -n "."
   sleep 1;
 done
-echo ""
 # wait for other part's public key - script won't run further until handshake finishes
 wait ${handshake_pid}
+clear
 
 # import key and ask to user confirm it
-partner_key=$(gpg --import "${publickeyfile}" 2>&1 | grep 'gpg: key' | grep -Eo '".*"' | tr -d '"')
-if [[ -z $partner_key ]]; then
-    read -p "Enter partner's Key ID (email or fingerprint): " partner_key
+partner_key_name=$(gpg --import "${publickeyfile}" 2>&1 | grep 'gpg: key' | grep -Eo '".*"' | tr -d '"')
+if [[ -z $partner_key_name ]]; then
+    read -p "Enter partner's Key ID (email or fingerprint): " partner_key_name
 fi
+partner_key_hash=$(gpg --list-keys $partner_key_name | head -2 | tail -1 | xargs)
 echo "🔒 Contact your partner over some other channel and compare your keys! It is important to prevent MITM attacks!"
-echo "🔒 ${partner_key}'s key is: $(gpg --list-keys $partner_key | head -2 | tail -1 | xargs)"
-echo "🔒 Your public key is: $(gpg --list-keys $username | head -2 | tail -1 | xargs)"
+echo "🔒 ${partner_key_name}'s key is: $partner_key_hash"
+echo "🔒 your public key is: $(gpg --list-keys $username | head -2 | tail -1 | xargs)"
 user_confirm=""
 until [[ "$user_confirm" =~ ^[YyNn]$ ]]; do
     read -n1 -p "Is this correct? (y/n): " user_confirm
     echo ""
 done
 if [[ "$user_confirm" =~ ^[Nn]$ ]]; then
-    echo "It is not secure to continue, if keys do not match. Exiting!"
+    echo "It is not secure to continue, if keys do not match."
+    read -n 1 -p "Press any button to exit" blank
     exit 255
 fi
-echo "✅ Partner key ready. Starting chat..."
+clear
+echo "✅ Partner key ready."
 echo ""
 
 echo ""
@@ -102,6 +107,7 @@ echo "🔑 Enter your passphrase once (cached by gpg-agent):"
 until echo test | gpg --encrypt --armor --recipient $username | gpg --decrypt --pinentry-mode=loopback &>/dev/null; do
   echo "🔑 Enter your passphrase once (cached by gpg-agent):"
 done
+clear
 echo "✅ Passphrase cached. Starting chat..."
 echo ""
 rm -f "$publickeyfile"
@@ -117,29 +123,36 @@ NC_PID=$!
 trap "kill $NC_PID; rm -f \"$chatpipe\" \"$publickeyfile\"; tput rmcup; tput sgr0; exit 0;" EXIT
 
 send_msg() {
-    encrypted=$(echo "${username}: $3" | gpg --encrypt --sign --armor --recipient "$partner_key" --local-user "$username" --trust-model always 2>/dev/null)
+    encrypted=$(echo "${username}: $3" | gpg --encrypt --sign --armor --recipient "$partner_key_name" --local-user "$username" --trust-model always 2>/dev/null)
     echo -e "$encrypted" | base64 -w 0 | nc -w 5 $host $port
     echo "" | nc -w 5 $host $port
     echo -ne "${BLUE}[$(date +%T)]${NC}${username}: "
 }
 
+tmpfile=$(mktemp -u 2>/dev/null || echo "/tmp/tmp.${RANDOM}${RANDOM}${RANDOM}")
+:>"$tmpfile" && chmod 600 "$tmpfile"
 receive_msg() {
     while read incoming_msg < "$chatpipe"; do
         decoded=$(echo "$incoming_msg" | base64 -d 2>/dev/null)
-        decrypted=$(echo "$decoded" | gpg --decrypt --pinentry-mode=loopback 2>/dev/null)
+        decrypted=$(echo "$decoded" | gpg --decrypt --pinentry-mode=loopback 2>"$tmpfile")
+        author_key_hash=$(grep "Primary key fingerprint:" "$tmpfile" | cut -d: -f2 | tr -d " ")
+        if [[ "$author_key_hash" != "$partner_key_hash" ]]; then
+            echo -ne "\n${RED}[$(date +%T)] WARNING! Partner identity has changed!${NC}"
+            echo -ne "\n${RED}[$(date +%T)] $partner_key_hash -> $author_key_hash${NC}"
+            echo -ne "\n${RED}[$(date +%T)] MITM possible! Do not ignore this message!${NC}"
+        fi
         if [[ -n "$decrypted" ]]; then
             echo -ne "\n${GREEN}[$(date +%T)]${NC}${decrypted}"
-            echo -ne "\n${BLUE}[$(date +%T)]${NC}${username}: "
         else
             echo -ne "\n${RED}[$(date +%T)] Failed to decrypt message or message is empty${NC}: $incoming_msg"
-            echo -ne "\n${BLUE}[$(date +%T)]${NC}${username}: "
         fi
+        echo -ne "\n${BLUE}[$(date +%T)]${NC}${username}: "
     done
 }
-echo -ne "${BLUE}[$(date +%T)]${NC}${username}: "
 # run receiving process in background
 receive_msg &
 
+echo -ne "${BLUE}[$(date +%T)]${NC}${username}: "
 while :; do
     read message
     send_msg "${host}" "${port}" "${message}"
